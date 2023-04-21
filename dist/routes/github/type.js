6f6dfe5cdb321b5591680f3a2e1afdf098270824
tigawanna <denniskinuthiaw@gmail.com>
2023-04-21 23:32:35 +0300

    removed dist

--- dist/routes/github/type.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pkgTypesArr = void 0;
-exports.pkgTypesArr = ["React+Vite", "React+Relay", "Rakkasjs", "Nextjs", "Nodejs", "Others"];
