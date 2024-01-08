3936f210a217475af45af8088e1629a417a2acf6
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-01-08 20:28:26 +0300

    init files

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,25 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import about from "./routes/about";
-import github from  './routes/github/github';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { logError, logNormal, logSuccess } from "./utils/helpers";
 
-import mongoose from 'mongoose';
-import { logError, logSuccess } from './utils/helpers';
+const PORT = 5000;
 
+const startServer = async () => {
+  dotenv.config();
 
-const ejs = require("ejs");
-const PORT=5000;    
+  const app: Express = express();
+  const port = process.env.PORT ? process.env.PORT : PORT;
+  app.use(express.json());
+  app.use(express.static("public"));
 
-const startServer=async()=>
-{
-    
-    dotenv.config();
-    
-    const app: Express = express();
-    const port = process.env.PORT?process.env.PORT:PORT;
-    app.use(express.json());
+  app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Hello World!" });
+  });
 
 
-  const PROD_MONGODB_URI = `mongodb+srv://denniskinuthiaw:${process.env.ATLAS_PWD}@frankfurt-cluster.htmeu8w.mongodb.net/?retryWrites=true&w=majority`
-  const LOCAL_MONGODB_URI = `mongodb://localhost:27017/green`
 
-  mongoose.connect(PROD_MONGODB_URI).then(
-      async() => {
-        logSuccess('MongoDb Connected!')
-
-      }
-      );
-
-    app.set("view engine", "ejs");
-    app.use(express.static("public"));
-
-    app.get('/', (req: Request, res: Response) => {
-      res.render("pages/index");
-    });
-
- 
-    app.use("/github",github);
-    app.use("/about",about);
-
-    app.listen(port, () => {
-        logSuccess(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-    });
-}
-startServer().catch(e=>logError("error starting server======== ",e))
+  app.listen(port, () => {
+  logNormal(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  });
+};
+startServer().catch((e) => logError("error starting server======== ", e));
--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-    res.send("main page ");
-});
